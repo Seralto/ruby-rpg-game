0c571bce800689174e9fb84b086c85275cc5caf7
Sérgio Toledo <contato@sergiotoledo.com.br>
2017-08-30 18:12:17 -0300

    Improve game play

--- game.rb
+++ game.rb
@@ -8,12 +8,21 @@ puts hero
 sword = Rpg::Weapon.new('Short sword', 3)
 hero.equip(sword)
 
-monster = Rpg::Enemy.new('Skull-en', 20, 3, 10)
-puts monster
+monsters_killed = 0
 
-while monster.alive? && hero.alive?
-  hero.attack(monster)
-  monster.attack(hero)
+while hero.alive?
+  puts '****** To battle! ******'
+  puts hero
+  monster = Rpg::Enemy.new('Skull-en', Random.rand(10) + 10, Random.rand(3) + 3, Random.rand(5) + 5)
+  puts monster
+
+  while monster.alive?
+    hero.attack(monster)
+    monster.attack(hero)
+  end
+
+  monsters_killed += 1 if hero.alive?
 end
 
-puts hero.alive? ? hero : monster
+puts "#{hero.name} is dead"
+puts "#{hero.name} killed #{monsters_killed} monsters"
