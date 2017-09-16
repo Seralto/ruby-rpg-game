ecb3895546bfe86ab0394a2a6c1e2826d6e6d61b
Sérgio Toledo <contato@sergiotoledo.com.br>
2017-09-16 01:34:14 -0300

    Add named params

--- lib/rpg/weapon.rb
+++ lib/rpg/weapon.rb
@@ -2,7 +2,7 @@ module Rpg
   class Weapon
     attr_reader :name, :damage
 
-    def initialize(name, damage)
+    def initialize(name:, damage:)
       @name = name
       @damage = damage
     end
