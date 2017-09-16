e5bcb2b5652f0a9e48d87688d7741708f206bbcc
Sérgio Toledo <contato@sergiotoledo.com.br>
2017-09-16 01:35:04 -0300

    Add named params and refactor

--- lib/rpg/potion.rb
+++ lib/rpg/potion.rb
@@ -2,14 +2,14 @@ module Rpg
   class Potion
     attr_reader :name, :healing_points
 
-    def initialize(name, healing_points)
+    def initialize(name:, healing_points:)
       @name = name
       @healing_points = healing_points
     end
 
     def heal(target)
       target.hp += @healing_points
-      puts "#{target.name} drinks the #{@name} and feels better\n\n"
+      puts "#{target.name} drinks the #{@name} and feels better!!!\n\n"
     end
   end
 end
