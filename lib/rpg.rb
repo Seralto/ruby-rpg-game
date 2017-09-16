0ff36ecedb9117b6712d9fb1c1cbec434c72d5b9
Sérgio Toledo <contato@sergiotoledo.com.br>
2017-09-16 01:36:24 -0300

    Create MONSTERS constant

--- lib/rpg.rb
+++ lib/rpg.rb
@@ -1,6 +1,9 @@
 Dir['./lib/rpg/*.rb'].each { |file| require file }
 
-require 'pry'
-
 module Rpg
+  MONSTERS = [
+    { name: 'Skull-in', hp: 20, strength: 5, exp: 10 },
+    { name: 'Werebat', hp: 12, strength: 4, exp: 6 },
+    { name: 'Deadtree', hp: 6, strength: 3, exp: 3 }
+  ].freeze
 end
